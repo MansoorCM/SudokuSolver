f526cbe12cb00c715cf374be5cfefc198afd4664
DELL <manzoorcm96@gmail.com>
2020-01-05 21:38:56 +0530

    Changed textcolor to bring more clarity to the visualization process.

--- app/src/main/java/com/example/sudokusolver/MainActivity.java
+++ app/src/main/java/com/example/sudokusolver/MainActivity.java
@@ -20,15 +20,16 @@ public class MainActivity extends AppCompatActivity {
     GridLayout sudoku;
     Handler mainHandler;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        board=new char[][]{{'5','3','4','6','7','8','9','1','2'},{'6','7','2','1','9','5','3','4','8'}
-//        ,{'1','9','8','3','4','2','5','6','7'},{'8','5','9','7','6','1','4','2','3'}
-//        ,{'4','2','6','8','5','3','7','9','1'},{'7','1','3','9','2','4','8','5','6'}
-//        ,{'9','6','1','5','3','7','2','8','4'},{'2','8','7','4','1','9','6','3','5'}
-//        ,{'3','4','5','2','8','6','1','7','9'}};
+//        board=new char[][]{{'.','.','9','7','4','8','.','.','.'},
+//        {'7','.','.','.','.','.','.','.','.'},{'.','2','.','1','.','9','.','.','.'},
+//        {'.','.','7','.','.','.','2','4','.'},{'.','6','4','.','1','.','5','9','.'},
+//        {'.','9','8','.','.','.','3','.','.'},{'.','.','.','8','.','3','.','2','.'},
+//        {'.','.','.','.','.','.','.','.','6'},{'.','.','.','2','7','5','9','.','.'}};
 
 
 
@@ -63,7 +64,17 @@ public class MainActivity extends AppCompatActivity {
     public void setvalue(char a,int index)
     {
         textView= (TextView) sudoku.getChildAt(index);
-        textView.setText(String.valueOf(a));
+        if(a==' ')
+        {
+            textView.setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+        else
+        {
+            textView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            textView.setText(String.valueOf(a));
+        }
+
+
     }
 
     class SolveHelper extends Thread{
